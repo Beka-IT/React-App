9fce91b7aafe2b79c667bc4aa7479bfeb831d2ee
Beka-IT <1904.01028@manas.edu.kg>
2023-09-03 00:07:45 -0400

    wrote ui of Sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { Articles } from './pages/Articles';
+import { SignIn } from './pages/SignIn';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+          <Route path="sign-in" element={<SignIn />} />
           <Route path="articles" element={<Articles />} />
       </Routes>
     </BrowserRouter>
